1b7d9bc934d19cedf40067974f6b907dc7f9ae59
Robert Luo <robertxluo@gmail.com>
2020-09-15 21:17:34 -0500

    Add navbar

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,20 @@
 import Head from 'next/head';
+import Nav from './Nav';
 
 export default function Home(props) {
   return (
     <div>
+      <Nav></Nav>
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>React Pinpoint</title>
       </Head>
       <body>
-        <div className="p-4 rounded shadow bg-white">
-          <h1 className="text-large text-purple-500 leading-normal">Next.js</h1>
-          <p className="text-gray-500">with Tailwind CSS</p>
-          <p className="text-gray-500">LETS GOOOOOO</p>
+        <div className="p-4 bg-white rounded shadow">
+          <h1 className="text-6xl font-semibold leading-normal text-center text-purple-500">React Pinpoint</h1>
+          <p className="text-center text-gray-600">with Tailwind CSS</p>
+          <p className="text-center text-gray-600">LETS GOOOOOO</p>
         </div>
       </body>
     </div>
