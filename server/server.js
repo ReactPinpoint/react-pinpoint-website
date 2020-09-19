58c334671c91a18f0db0a8fbffe428f74c53a93e
Jeffrey Lu <yumyumyum@yumyumyum-mothership.local>
2020-09-18 20:54:49 -0400

    sets up server routes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const { Sequelize } = require('sequelize');
 
@@ -7,32 +8,39 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const dbname = process.env.DB_NAME;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const dbport = process.env.DB_PORT
-
-const sequelize = new Sequelize(dbname, username, password, {
-  host: process.env.DB_URL,
-  port: dbport,
-  dialect: 'postgres',
-  ssl: 'Amazon RDS',
-});
-
-
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Welcome to reactpp API'));
+const userRouter = require('./routes/user');
+const projectRouter = require('./routes/project');
+const analyticsRouter = require('./routes/analytics');
+app.use('/api/user', userRouter);
+app.use('/api/project', projectRouter);
+app.use('/api/analytics', analyticsRouter);
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connected to DB');
-    app.listen(PORT, () => {
-      console.log(`Server running at port ${PORT}`);
-    })
-  } catch (error) {
-    console.log(error);
-  }
+
+if (process.env.NODE_ENV === 'development') {
+  app.get('/', (req, res) => {
+    res.send('Welcome to reactpp API');
+  });
 }
-)();
+else if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.NODE_ENV)
+  app.use(express.static(path.join(__dirname, '../out')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../out/index.html'));
+  });
+}
+
+// global error handler
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(err);
+    console.log('Error:', err);
+  }
+  return res.status(400).json(err);
+});
+
+// run the server
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+})
