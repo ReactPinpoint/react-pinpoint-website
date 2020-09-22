568641913765d2fb73e7ec57bc114625f19d5727
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-09-21 22:43:53 -0400

    adds authentication middleware to / route

--- server/server.js
+++ server/server.js
@@ -1,35 +1,33 @@
 const express = require('express');
-const path = require('path');
 const dotenv = require('dotenv');
-const { Sequelize } = require('sequelize');
+const passport = require('passport');
+const session = require('express-session');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const isAuthorized = require('./auth/auth');
+
 app.use(express.json({ extended: false }));
+app.use(session({ secret: process.env.SECRET }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 const userRouter = require('./routes/user');
 const projectRouter = require('./routes/project');
 const analyticsRouter = require('./routes/analytics');
+const { addHook } = require('./sequelize/models/user');
 app.use('/api/user', userRouter);
 app.use('/api/project', projectRouter);
 app.use('/api/analytics', analyticsRouter);
 
-
-if (process.env.NODE_ENV === 'development') {
-  app.get('/', (req, res) => {
-    res.send('Welcome to reactpp API');
-  });
-}
-else if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV)
-  app.use(express.static(path.join(__dirname, '../out')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../out/index.html'));
-  });
-}
+app.get('/', 
+  isAuthorized,
+  (req, res) => {
+  res.send('Welcome to reactpp API');
+});
 
 // global error handler
 app.use((err, req, res, next) => {
