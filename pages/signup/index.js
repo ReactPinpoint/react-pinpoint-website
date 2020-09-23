33a8d1949409c96f6d8a72f9a3159369e738b9ab
Robert Luo <robertxluo@gmail.com>
2020-09-23 16:03:17 -0500

    Install http-proxy-middlware

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -52,7 +52,7 @@ export default function SignUp() {
             <label htmlFor="email">Email</label>
             <input
               ref={register({
-                required: 'Email is required.',
+                required: 'Email is required',
                 pattern: {
                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                   message: 'Please enter a valid email',
