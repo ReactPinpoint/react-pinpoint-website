d5029600f83a1f063957c50ae3477684998d1d07
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-09-24 22:00:31 -0400

    refactors user controller to get users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,10 @@
 const User = require('../sequelize/models/user');
-const UserProject = require('../sequelize/models/user-projects');
 
 const userController = {};
 
 userController.getUser = async (req, res, next) => {
   try {
-    const { username } = req.params;
-    const user = await User.findOne({ 
-      where: {
-        username
-      }
-    });
+    const user = await User.findOne({ where: { user_id: res.locals.user_id } });
     res.locals.user = user;
     next();
   } catch (err) {
@@ -18,60 +12,10 @@ userController.getUser = async (req, res, next) => {
   }
 }
 
-userController.register = async (req, res, next) => {
+userController.getUsers = async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const results = await User.findAll({ where: { username }});
-    if (results.length) {
-      res.locals.createdUser = { err: 'User exists'};
-      return next();
-    }
-    const user = await User.create({ username, password })
-    res.locals.createdUser = user;
-    // TODO start a session
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
-
-userController.updateUser = async (req, res, next) => {
-  try {
-    // TODO write logic to update user in database
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
-
-userController.deleteUser = async (req, res, next) => {
-  // this just deletes all users for now
-  try {
-    User.destroy({ where: {} });
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
-
-userController.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      res.locals.loggedIn = { loggedIn: false };
-      return next();
-    }
-    const user = await User.findOne({
-      where: {
-        username,
-      }
-    })
-    if (!user) {
-      res.locals.loggedIn = { loggedIn: false };
-      return next();
-    }
-    const validated = await user.validatePassword(password, user);
-    res.locals.loggedIn = { loggedIn: validated };
+    const users = await User.findAll({ where: {} });
+    res.locals.users = users;
     next();
   } catch (err) {
     next(err);
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,29 +4,14 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/getuser/:username',
+router.get('/',
   userController.getUser,
   (req, res) => res.status(200).json(res.locals.user)
 );
 
-router.post('/register', 
-  userController.register,
-  (req, res) => res.status(200).json(res.locals.createdUser)
-);
-
-router.put('/updateuser',
-  userController.deleteUser,
-  (req, res) => res.status(200).json({})
-);
-
-router.delete('/deleteuser',
-  userController.deleteUser,
-  (req, res) => res.status(200).json({})
-);
-
-router.post('/login', 
-  userController.login,
-  (req, res) => res.status(200).json(res.locals.loggedIn)
+router.get('/all',
+  userController.getUsers,
+  (req, res) => res.status(200).json(res.locals.users)
 )
 
 module.exports = router;
