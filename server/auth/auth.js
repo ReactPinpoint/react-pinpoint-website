40afa8e028f753b3d70e815626b6b0a461c98773
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-09-24 22:00:20 -0400

    adds authentication via jwts

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,13 +1,21 @@
 const User = require('../sequelize/models/user');
+const jwt = require('jsonwebtoken');
 
 const isAuthorized = async (req, res, next) => {
   try {
-    const { user_id } = req.session;
+    const { token } = req.cookies;
+    const user_id = jwt.verify(
+      token,
+      process.env.SECRET,
+      (err, decoded) => {
+        if (err) return next(err);
+        return decoded.user_id;
+      }
+    )
+    if (!user_id) return next('Not Authorized.');
     const user = await User.findByPk(user_id);
-    if (!user) {
-      const err = 'Not Authorized.'
-      return next(err);
-    }
+    if (!user) return next('Not Authorized.');
+    res.locals.user_id = user.user_id;
     next();
   } catch (err) {
     next(err);
