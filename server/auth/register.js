40654b30fc4f88ee3abccffd2d0b23df8b814939
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-09-24 22:00:51 -0400

    adds register functionality

---
+++ server/auth/register.js
@@ -0,0 +1,17 @@
+const register = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const results = await User.findAll({ where: { username }});
+    if (results.length) {
+      res.locals.newUser = { err: 'User exists'};
+      return next();
+    }
+    const user = await User.create({ username, password })
+    res.locals.newUser = { user_id: user.user_id };
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = register;
