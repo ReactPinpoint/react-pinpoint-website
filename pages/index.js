f5cedc92cb5b3f24af56ede553f0ef7f318f80ce
Robert Luo <robertxluo@gmail.com>
2020-09-29 20:14:58 -0500

    Revert to previous commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Nav from 'components/nav';
-import Container from 'components/container';
-import Button from 'components/button';
+import Nav from '../components/nav';
+import Container from '../components/container';
+import Button from '../components/button';
 
 export default function Home() {
   return (
