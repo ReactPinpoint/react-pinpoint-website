b00da717e917b2e4203cec0c47ceb68024a047bf
Robert Luo <robertxluo@gmail.com>
2020-09-29 19:59:37 -0500

    Add jsx file extensions in import statements

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Nav from '../components/nav';
-import Container from '../components/container';
-import Button from '../components/button';
+import Nav from '../components/nav.jsx';
+import Container from '../components/container.jsx';
+import Button from '../components/button.jsx';
 
 export default function Home() {
   return (
