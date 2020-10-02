08cd25ce22ef1436d914281ebecb69549e95c3d3
Robert Luo <robertxluo@gmail.com>
2020-10-02 01:56:09 -0500

    Make landing page responsive

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,13 @@ export default function Home() {
       <div>
         <Nav></Nav>
 
-        <div className="flex py-16 justify-evenly bg-primary-100">
-          <div className="max-w-xs pt-24">
+        <div className="flex flex-col px-10 md:pl-64 justify-evenly bg-primary-100">
+          <div className="max-w-xs md:pt-24">
             <h1 className="text-4xl font-semibold leading-tight text-primary-1000">Testing smarts for React applications</h1>
-            <h2 className="pt-4 text-base leading-normal text-primary-600">
+            <h2 className="pt-8 text-lg leading-normal md:pt-4 md:text-base text-primary-600">
               React Pinpoint helps developers locate performance bottlenecks on their React components.
             </h2>
-            <div className="mt-6">
+            <div className="mt-6 mb-6">
               <Link href="/signup">
                 <a>
                   <Button>Get Started </Button>
@@ -29,7 +29,7 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          <div className="">
+          <div className="pb-6">
             <img className="object-scale-down w-full h-full max-w-lg max-h-lg sm:hidden" src="/hero-image.png" alt="React" />
           </div>
         </div>
