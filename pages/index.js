b5c702d1c2bfc386d67620cdd9e8664e20d39f81
Robert Luo <robertxluo@gmail.com>
2020-10-02 10:19:46 -0500

    Style contribute section

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,10 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="flex flex-col items-center pt-8 mx-4 bg-neutral-100">
+        <section className="flex flex-col items-center pt-8 mx-4 md:pl-64 md:pr-64 md:mx-auto bg-neutral-100">
           <h2 className="text-2xl text-primary-1000">Meet the team.</h2>
-          <div className="flex flex-col xl:flex-row xl:justify-evenly xl:pt-12">
-            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mx-8">
+          <div className="flex flex-col xl:flex-row xl:justify-between xl:pt-4">
+            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mr-8">
               <div className="flex flex-col items-center px-6 py-4">
                 <div className="my-2 text-xl font-bold text-primary-1000">Tai Nguyen</div>
                 <a href="https://github.com/ndhuutai" target="_blank">
@@ -53,7 +53,7 @@ export default function Home() {
                 <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#winter</span>
               </div>
             </div>
-            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mx-8">
+            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mr-8">
               <div className="flex flex-col items-center px-6 py-4">
                 <div className="my-2 text-xl font-bold text-primary-1000">Jeffrey C. Lu</div>
                 <a href="https://github.com/jeffreyclu" target="_blank">
@@ -69,7 +69,7 @@ export default function Home() {
                 <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#winter</span>
               </div>
             </div>
-            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mx-8">
+            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mr-8">
               <div className="flex flex-col items-center px-6 py-4">
                 <div className="my-2 text-xl font-bold text-primary-1000">Matthew Batelic</div>
                 <a href="https://github.com/batelicm" target="_blank">
@@ -85,7 +85,7 @@ export default function Home() {
                 <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#winter</span>
               </div>
             </div>
-            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg xl:mx-8">
+            <div className="max-w-sm mx-auto my-8 overflow-hidden bg-white rounded-lg shadow-lg">
               <div className="flex flex-col items-center px-6 py-4">
                 <div className="my-2 text-xl font-bold text-primary-1000">Robert Luo</div>
                 <a href="https://github.com/robertxluo" target="_blank">
@@ -101,6 +101,23 @@ export default function Home() {
                 <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#winter</span>
               </div>
             </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <h2 className="mt-8 text-2xl text-primary-1000">Want to contribute?</h2>
+            <p className="mt-4 text-primary-600">React Pinpoint is open source.</p>
+            <p className="text-primary-600">Help make React Pinpoint better!</p>
+            <a href="https://github.com/oslabs-beta/react-pinpoint" target="_blank">
+              <button className="flex px-4 py-2 my-8 font-semibold border border-gray-400 rounded shadow focus:outline-none hover:bg-primary-400 bg-primary-500 text-neutral-100">
+                GitHub{' '}
+                <svg className="ml-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path
+                    d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                    fill="#F0F4F8"
+                    class="color000 svgShape"
+                  ></path>
+                </svg>
+              </button>
+            </a>
           </div>
         </section>
       </div>
