797b5049dfb556efec64e618bcef71c13c4580db
Robert Luo <robertxluo@gmail.com>
2020-10-01 22:17:12 -0500

    Style login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -34,15 +34,21 @@ export default function Login() {
     }
   };
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-100 ">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-100">
       <Link href="/">
-        <a className="mb-4">React Pinpoint Logo Goes Here</a>
+        <a className="mb-4">
+          <svg className="w-8 h-8 mr-2 fill-current" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+        </a>
       </Link>
-      <div className="w-full max-w-sm px-8 py-8 text-black bg-white rounded shadow-md">
-        <h1 className="flex flex-row text-lg text-center">Sign in to your account</h1>
+      <div className="w-full max-w-md p-12 bg-white rounded shadow-md text-neutral-1000">
+        <h1 className="flex flex-row mb-4 text-xl font-light text-center">Sign in to your account</h1>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col" noValidate>
           <div className="mt-2 mb-4">
-            <label htmlFor="email">Email</label>
+            <label className="text-sm" htmlFor="email">
+              Email
+            </label>
             <input
               ref={register({
                 required: 'Email is required',
@@ -50,13 +56,15 @@ export default function Login() {
               name="username"
               id="email"
               type="email"
-              className="block w-full p-2 border rounded border-grey-light"
+              className="block w-full p-2 mt-2 border rounded border-grey-light"
             />
-            <p className="text-red-600">{errors.username && errors.username.message}</p>
+            <p className="text-sm text-red-600">{errors.username && errors.username.message}</p>
           </div>
 
-          <div className="mb-4">
-            <label htmlFor="password">Password</label>
+          <div className="mt-4 mb-8">
+            <label className="text-sm" htmlFor="password">
+              Password
+            </label>
             <input
               ref={register({
                 required: 'Password is required',
@@ -64,21 +72,21 @@ export default function Login() {
               name="password"
               id="password"
               type="password"
-              className="block w-full p-2 border rounded border-grey-light"
+              className="block w-full p-2 mt-2 border rounded border-grey-light"
             />
-            <p className="text-red-600">{errors.password && errors.password.message}</p>
-            <p className="text-red-600">{passwordError}</p>
+            <p className="text-sm text-red-600 ">{errors.password && errors.password.message}</p>
+            <p className="text-sm text-red-600">{passwordError}</p>
           </div>
-          <button type="submit" className="w-full py-3 my-1 text-center text-white bg-purple-400 rounded hover:bg-purple-300 focus:outline-none">
+          <button type="submit" className="w-full py-3 my-1 text-center text-white rounded bg-primary-1000 hover:bg-primary-900 focus:outline-none">
             Continue
           </button>
           <p className="mt-2 text-red-600">{miscError}</p>
         </form>
       </div>
-      <p className="mt-4">
+      <p className="mt-8 text-sm text-primary-1000">
         Don't have an account?{' '}
         <Link href="/signup">
-          <a className="text-blue-500 underline">Sign Up</a>
+          <a className="font-light text-primary-600 hover:text-primary-1000">Sign Up</a>
         </Link>
       </p>
     </div>
