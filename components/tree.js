d19bc80ff81e25582cfd11e2349165f16adc122f
Robert Luo <robertxluo@gmail.com>
2020-10-02 12:58:43 -0500

    Add canUseDom util function

--- components/tree.js
+++ components/tree.js
@@ -1,5 +1,11 @@
-import Tree from 'react-d3-tree';
+import canUseDom from '../utils/can-use-dom';
+if (canUseDOM()) {
+  const ReactD3Tree = require('react-d3-tree');
+  Tree = ReactD3Tree.default;
+}
 
 export default function TreeComponent({ ...treeData }) {
+  if (!canUseDom()) return null;
+
   return <Tree data={treeData} orientation="vertical" />;
 }
