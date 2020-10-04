9b5682a0f4bee797ad7ee26678260c758fce7892
Robert Luo <robertxluo@gmail.com>
2020-10-04 16:06:06 -0500

    Conditionally change nav bar color

--- components/nav.js
+++ components/nav.js
@@ -20,13 +20,30 @@ export default function Nav({ ...props }) {
     }
   };
   return (
-    <nav className="flex flex-col items-center justify-between py-4 md:px-64 lg:flex-row bg-primary-100">
+    <nav
+      className={
+        loggedIn
+          ? `flex flex-col items-center justify-between py-4 md:pl-8 md:pr-64 lg:flex-row bg-white`
+          : `flex flex-col items-center justify-between py-4 md:px-64 lg:flex-row bg-primary-100`
+      }
+    >
       <div className="flex items-center flex-shrink-0 mr-6 text-neutral-1000">
-        <svg className="w-8 h-8 mr-2 fill-current" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
         <Link href={loggedIn ? '/dashboard' : '/'}>
-          <a className="text-4xl font-semibold tracking-tight cursor-pointer md:text-2xl">React Pinpoint</a>
+          <a>
+            <svg
+              className="w-8 h-8 mr-2 fill-current text-primary-1000 hover:text-primary-800"
+              width="54"
+              height="54"
+              viewBox="0 0 54 54"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+            </svg>
+          </a>
+        </Link>
+
+        <Link href={loggedIn ? '/dashboard' : '/'}>
+          <a className="text-4xl font-semibold tracking-tight cursor-pointer text-primary-1000 hover:text-primary-800 md:text-2xl">React Pinpoint</a>
         </Link>
       </div>
 
