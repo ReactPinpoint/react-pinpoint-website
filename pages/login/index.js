a329276c612168565dd56a96d2d3a418096c9876
Robert Luo <robertxluo@gmail.com>
2020-10-06 12:21:29 -0500

    Enter email validation in front end for login

--- pages/login/index.js
+++ pages/login/index.js
@@ -52,6 +52,10 @@ export default function Login() {
             <input
               ref={register({
                 required: 'Email is required.',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Please enter a valid email.',
+                },
               })}
               name="username"
               id="email"
