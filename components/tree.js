2a23a9eb80c308e21808d7f06bcf1bdbc9a6cd44
Robert Luo <robertxluo@gmail.com>
2020-10-07 02:21:00 -0500

    Style links for tree

--- components/tree.js
+++ components/tree.js
@@ -1,5 +1,18 @@
 import Tree from 'react-d3-tree';
 
 export default function TreeComponent({ treeData }) {
-  return <Tree data={treeData} orientation="horizontal" transitionDuration={0} collapsible={false} />;
+  return (
+    <Tree
+      data={treeData}
+      orientation="horizontal"
+      transitionDuration={0}
+      collapsible={false}
+      styles={{
+        links: {
+          stroke: '#5850ec',
+          strokeWidth: 4,
+        },
+      }}
+    />
+  );
 }
