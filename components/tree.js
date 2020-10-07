b54fe433182299e0f6924ad29001552ce173d7cc
Robert Luo <robertxluo@gmail.com>
2020-10-07 02:15:38 -0500

    Remove unused import

--- components/tree.js
+++ components/tree.js
@@ -1,14 +1,5 @@
-import canUseDOM from '../utils/can-use-dom';
 import Tree from 'react-d3-tree';
 
 export default function TreeComponent({ treeData }) {
-
-  return (
-    <Tree 
-      data={treeData} 
-      orientation="horizontal"
-      transitionDuration={0}
-      collapsible={false}
-    />
-  );
+  return <Tree data={treeData} orientation="horizontal" transitionDuration={0} collapsible={false} />;
 }
