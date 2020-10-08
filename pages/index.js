ecab98935c4763069202ee18ea9cd3188acf289f
Robert Luo <robertxluo@gmail.com>
2020-10-08 15:28:45 -0500

    Add screenshot to landing page

--- pages/index.js
+++ pages/index.js
@@ -269,6 +269,97 @@ export default function Home() {
           </div>
         </section>
 
+        <div className="py-16 overflow-hidden bg-gray-50 lg:py-24">
+          <div className="relative max-w-xl px-4 mx-auto sm:px-6 lg:px-8 lg:max-w-screen-xl">
+            <svg
+              className="absolute hidden transform -translate-x-1/2 lg:block left-full -translate-y-1/4"
+              width="404"
+              height="784"
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern id="svg-pattern-squares-1" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="784" fill="url(#svg-pattern-squares-1)" />
+            </svg>
+
+            <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+              <div className="relative">
+                <h4 className="text-3xl font-bold leading-8 tracking-tight text-neutral-900 sm:text-3xl sm:leading-10">
+                  Your Components, Visualized
+                </h4>
+                <p className="mt-3 text-lg leading-7 text-neutral-700">
+                  React Pinpoint color codes all your React components based on the render time, allowing you to pinpoint exactly which components are
+                  unperformant.
+                </p>
+              </div>
+
+              <div className="relative mt-10 -mx-4 lg:mt-0">
+                <svg
+                  className="absolute transform -translate-x-1/2 translate-y-16 left-1/2 lg:hidden"
+                  width="784"
+                  height="404"
+                  fill="none"
+                  viewBox="0 0 784 404"
+                >
+                  <defs>
+                    <pattern id="svg-pattern-squares-2" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                      <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                    </pattern>
+                  </defs>
+                  <rect width="784" height="404" fill="url(#svg-pattern-squares-2)" />
+                </svg>
+                <img className="relative mx-auto" width="490" src="/bigtree-framed.png" alt="Tree of React Components" />
+              </div>
+            </div>
+
+            <svg
+              className="absolute hidden transform translate-x-1/2 translate-y-12 lg:block right-full"
+              width="404"
+              height="784"
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern id="svg-pattern-squares-3" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="784" fill="url(#svg-pattern-squares-3)" />
+            </svg>
+
+            <div className="relative mt-12 sm:mt-16 lg:mt-24">
+              {/* <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+                <div className="lg:col-start-2">
+                  <h4 className="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-3xl sm:leading-9">Always in the loop</h4>
+                  <p className="mt-3 text-lg leading-7 text-gray-500">See all your React components in a tree.</p>
+                </div>
+
+                <div className="relative mt-10 -mx-4 lg:mt-0 lg:col-start-1">
+                  <svg
+                    className="absolute transform -translate-x-1/2 translate-y-16 left-1/2 lg:hidden"
+                    width="784"
+                    height="404"
+                    fill="none"
+                    viewBox="0 0 784 404"
+                  >
+                    <defs>
+                      <pattern id="svg-pattern-squares-4" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                      </pattern>
+                    </defs>
+                    <rect width="784" height="404" fill="url(#svg-pattern-squares-4)" />
+                  </svg>
+                  <img className="relative mx-auto" width="490" src="/img/features/feature-example-2.png" alt="" />
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </div>
+
         <section>
           <div className="bg-white">
             <div className="max-w-screen-xl px-4 py-12 mx-auto text-center sm:px-6 lg:px-8 lg:py-24">
