a2dc6bbb4bb5ba5dfbbf3db61aa2a7d5b2bddab7
Robert Luo <robertxluo@gmail.com>
2020-10-08 17:39:55 -0500

    Style change buttons

--- components/treemenu.js
+++ components/treemenu.js
@@ -1,20 +1,22 @@
 /* eslint-disable react/no-array-index-key */
-const Change = ({ changeIndex, setChangeIndex, buttonIndex }) => (
+const Change = ({ changeIndex, setChangeIndex, buttonId }) => (
   <div className="flex flex-col items-center w-auto ">
     <button
       type="button"
       className={
-        buttonIndex === changeIndex
-          ? 'px-2 py-1 mx-4 my-2 text-xs font-medium leading-5 bg-indigo-700 border border-b-4 rounded-md shadow outline-none focus:outline-none focus:shadow-xl text-white focus:border-indigo-800 hover:bg-neutral-100 focus:bg-indigo-700 focus:text-white'
-          : 'px-2 py-1 mx-4 my-2 text-xs font-medium leading-5 bg-white border border-b-4 rounded-md shadow outline-none focus:outline-none focus:shadow-xl text-neutral-1000 focus:border-indigo-800 hover:bg-neutral-100 focus:bg-indigo-700 focus:text-white'
+        buttonId === changeIndex
+          ? 'px-2 py-1 mx-4 my-2 text-xs font-medium leading-5 bg-indigo-700 border-indigo-800 border border-b-4 rounded-md  focus:outline-none  shadow-xl text-white'
+          : 'px-2 py-1 mx-4 my-2 text-xs font-medium leading-5 bg-white border border-b-4 rounded-md shadow outline-none text-neutral-1000 focus:outline-none  hover:bg-neutral-100'
       }
-      onClick={() => setChangeIndex(buttonIndex)}
+      onClick={() => setChangeIndex(buttonId)}
     >
-      Change {buttonIndex}
+      Change {buttonId}
     </button>
   </div>
 );
 export default function TreeMenu({ changes, setChangeIndex, changeIndex }) {
-  const changeList = changes.map((change, i) => <Change key={`Change ${i}`} buttonIndex={i} setChangeIndex={setChangeIndex} changeIndex={changeIndex} />);
+  const changeList = changes.map((change, i) => (
+    <Change key={`Change ${i}`} buttonId={i} setChangeIndex={setChangeIndex} changeIndex={changeIndex} />
+  ));
   return <div className="border-r-4 border-neutral-700 bg-neutral-300">{changeList}</div>;
 }
